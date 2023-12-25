c1d73bcbe90c16d308049e9dd924a666d379c932
Daniel Ebert <iuq@web.de>
2023-12-25 21:50:32 +0100

    changed files

--- fuzzer/fuzzer.py
+++ fuzzer/fuzzer.py
@@ -21,19 +21,31 @@ def build_executables() -> list[str]:
 
     return [os.path.normpath(os.path.join(build_dir, f)) for f in ['exe_a', 'exe_b']]
 
+def get_changed_files() -> list[str]:
+    base_commit = subprocess.check_output(['git', 'merge-base', 'HEAD', 'main'], text=True).strip()
+    paths = subprocess.check_output(['git', 'diff', '--name-only', f'{base_commit}..'], text=True).splitlines()
+    return [os.path.basename(path) for path in paths]
+
+
+def executable_has_changed(executable_path: str, diff_changed_files: list[str]) -> bool:
+    exe_changed_paths = used_files.get_used_files(executable_path)
+    exe_changed_files = [os.path.basename(path) for path in exe_changed_paths]
+    return set(exe_changed_files) & set(diff_changed_files)
 
 def main(argv: Sequence[str] | None = None) -> int:
     print('Running fuzzer')
 
     executable_paths = build_executables()
-
-    for executable_path in executable_paths:
-        used_elf_files = used_files.get_used_files(executable_path)
-        print(f'{used_elf_files=}')
     
     # i know that path starting from /*/HERE is always the same, no matter where its build. /*/ is the subsys name
 
-    print(executable_paths)
+    diff_changed_files = get_changed_files()
+
+    print(get_changed_files())
+
+    changed_exes = [exe for exe in executable_paths if executable_has_changed(exe, diff_changed_files)]
+
+    print(f'{changed_exes=}')
 
     return 0
 
